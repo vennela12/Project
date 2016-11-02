b2ebde6775152bcaec764664b3e52a5202e2169b
vennela12 <vennela12@gmail.com>
2016-11-01 21:47:58 -0500

    added database project

---
+++ MGProject/MGDB/tUser.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[tUser]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Username] VARCHAR(16) NOT NULL, 
+    [Password] NVARCHAR(16) NOT NULL, 
+    [FirstName] VARCHAR(64) NOT NULL, 
+    [MiddleName] VARCHAR(32) NULL, 
+    [LastName] VARCHAR(64) NOT NULL
+)
